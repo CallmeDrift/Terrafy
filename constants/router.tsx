ecbdfc38e9faeef0146299757b337a409f63f8e5
Drift <diego.suarez1637@gmail.com>
2026-04-11 14:35:04 -0500

    feat: update API_URL to local development server

--- constants/router.tsx
+++ constants/router.tsx
@@ -1,1 +1,3 @@
-export const API_URL = "https://terrafy-api.braveflower-b50c6491.switzerlandnorth.azurecontainerapps.io/api";
+export const API_URL = "http://192.168.1.7:3000/api";
+
+//https://terrafy-api.braveflower-b50c6491.switzerlandnorth.azurecontainerapps.io
